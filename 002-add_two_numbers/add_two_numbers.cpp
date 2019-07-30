5b49ac4de5e0f81872f6165f79cdda7920e05170
Felix <felixwiberg@gmail.com>
2019-07-30 17:08:43 +0100

    add_two_numbers.cpp

--- 002-add_two_numbers/add_two_numbers.cpp
+++ 002-add_two_numbers/add_two_numbers.cpp
@@ -24,45 +24,58 @@ class Solution {
         ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
             ListNode *cur = new ListNode(0);
             ListNode *ret = cur;
-            int carry = 0;
+            
+            int carry = 0,
+                val_to_next = 0,
+                tmp_sum = 0;
 
             ListNode *c1 = l1;
             ListNode *c2 = l2;
-            while(c1->next != NULL){
-                cur->next = new ListNode(0);
-                if(c1->val+c2->val+carry >= 10){
-                    cur->val = (c1->val+c2->val+carry) % 10;
-                    carry = 1;
-                }else{
-                    cur->val = (c1->val+c2->val+carry);
-                    carry = 0;
-                }
-                cur = cur->next;
-                c1 = c1->next;
-                c2 = c2->next;
-            }
             
-            if(c1->val+c2->val+carry >= 10){
-                cur->val = (c1->val+c2->val+carry) % 10;
-                cur->next = new ListNode((c1->val+c2->val+carry)/10);
-            }else{
-                cur->val = (c1->val+c2->val);
+            while(c1 != NULL || c1 != NULL || carry > 0){
+                tmp_sum = 0;
+                if(c1 != NULL){
+                    tmp_sum += c1->val;
+                    c1 = c1->next;
+                }
+                if(c2 != NULL){
+                    tmp_sum += c2->val;
+                    c2 = c2->next;
+                }
+                tmp_sum += carry;
+                carry = tmp_sum/10;
+                val_to_next = tmp_sum%10;
+                cur->val = val_to_next;
+                cur->next = new ListNode(0);
+                cur = cur->next;
             }
-
+            cur = NULL;
             return ret;
         }
 };
 
+void printllist(ListNode* l){
+    printf("Linked list: ");
+    while(l != NULL){
+        printf(" (%d)", l->val);
+        l = l->next;
+    }
+    printf("\n");
+}
+
 int main(){
-    ListNode *l1 = new ListNode(5);
-    l1->next = new ListNode(5);
+    ListNode *l1 = new ListNode(1);
+    l1->next = new ListNode(1);
     l1->next->next = new ListNode(5);
 
-    ListNode *l2 = new ListNode(5);
-    l2->next = new ListNode(5);
+    ListNode *l2 = new ListNode(1);
+    l2->next = new ListNode(1);
     l2->next->next = new ListNode(5);
 
     Solution s;
+    printllist(l1);
+    printllist(l2);
     ListNode *ret = s.addTwoNumbers(l1, l2);
+    printllist(ret);
     return 0;
 }
