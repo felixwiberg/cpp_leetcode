1cb81bca95fd30797ef729d45a3d20ca0bab9121
Felix <felixwiberg@gmail.com>
2019-07-31 15:42:48 +0100

    longest_substring.cpp

--- 003-longest_substring/longest_substring.cpp
+++ 003-longest_substring/longest_substring.cpp
@@ -4,20 +4,18 @@ using namespace std;
 
 class Solution{
     public:
-        int lengthOfLongestSubstring(string s){
-            
+        int lengthOfLongestSubstring(string s){ 
             vector<int> ascii(128, 0);
-            int tmp_sum = 0, longest_substring = 0;
-
-            for(int i = 0; i<s.length(); i++){
-                for(int j = i; j<s.length(); j++){
+            int tmp_sum = 0, longest_substring = 0, len = s.length();
+            if(len==1){return 1;}
+            for(int i = 0; i<len; i++){
+                for(int j = i; j<len; j++){
                     if(ascii[s[j]] == 0){
                         ascii[s[j]] = 1;
                         tmp_sum += 1;
                     }else{
                         if(tmp_sum > longest_substring){ longest_substring = tmp_sum; }
                         tmp_sum = 0;
-                        j = 128;
                         fill(ascii.begin(), ascii.end(), 0);
                         break;
                     }
@@ -29,7 +27,7 @@ class Solution{
 
 int main(){
     Solution s;
-    string str = "bb";
+    string str = "a";
     int ret = s.lengthOfLongestSubstring(str);
     cout << "Longest substring in " << str << " is: " << ret << endl;
     return 0;
