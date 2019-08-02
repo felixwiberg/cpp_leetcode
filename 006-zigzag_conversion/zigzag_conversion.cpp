3e543873bfc4e6f3b425989c3fd16ea5f6ea935c
Felix <felixwiberg@gmail.com>
2019-08-02 21:27:39 +0100

    zigzag_conversion.cpp

---
+++ 006-zigzag_conversion/zigzag_conversion.cpp
@@ -0,0 +1,39 @@
+#include<string>
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+class Solution{
+    public:
+        string convert(string s, int numRows){
+            int len = s.size();
+            int dir = -1;
+            int row = 0;
+
+            string arr[numRows];
+            for(int i = 0; i<len; i++){
+                arr[row].push_back(s[i]);
+                if(row == numRows-1){
+                    dir = -1;
+                }
+                if(row == 0){
+                    dir = 1;
+                }
+                row += dir;
+            }
+            string ret;
+            for(int j = 0; j<numRows; j++){
+                ret += arr[j];
+            }
+            return ret;
+        }
+};
+
+int main(){
+    Solution s;
+    string str = "hejjagheterfelix";
+    string ret = s.convert(str, 3);
+    cout << ret <<endl;
+    return 0;
+}
