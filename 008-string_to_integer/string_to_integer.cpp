e157bbbed88838dde4311e854aba1a314915b9e7
Felix <felixwiberg@gmail.com>
2019-08-12 17:47:57 +0100

    string_to_integer.cpp

--- 008-string_to_integer/string_to_integer.cpp
+++ 008-string_to_integer/string_to_integer.cpp
@@ -42,7 +42,7 @@ class Solution{
 
 int main(){
     Solution s;
-    string str = "-+1";
+    string str = " -1";
     //string str = "991283472332";
     //string str = "    -142 hej";
     int ret = s.myAtoi(str);
