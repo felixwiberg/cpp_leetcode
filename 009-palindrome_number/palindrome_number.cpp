43309b9977e9c9bd1af6e92e5789f0729c06440b
Felix <felixwiberg@gmail.com>
2019-08-14 13:45:09 +0100

    palindrome_number

--- 009-palindrome_number/palindrome_number.cpp
+++ 009-palindrome_number/palindrome_number.cpp
@@ -6,31 +6,19 @@ using namespace std;
 class Solution {
     public:
         bool isPalindrome(int x){
-            if(x < 0){ return false; }
-            if(x < 10 && x >= 0){ return true; }
-            int c = x;
-            int n = 0;
-            while(c > 0){
-                c /= 10;
-                n++;
-            }
-            int array[n];
-            for(int i = n-1; i>=0; i--){
-                array[i] = x%10;
+            int z = x;
+            int y = 0;
+            while(x){
+                y = y*10 + x%10;
                 x /= 10;
             }
-            for(int i = 0; i<n/2; i++){
-                if(!(array[i] == array[n-i-1])){
-                    return false;
-                }
-            }
-            return true;
+            return z == y? true:false;
         }
 };
 
 int main(){
     Solution s;
-    int x = 11011;
+    int x = 1;
     bool ret = s.isPalindrome(x);
     cout << "palindrome? : " << ret << endl;
     return 0;
